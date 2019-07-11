e6ea14187a8ebf475294273ebf4987ff9505ac11
libing070 <18680506315@163.com>
2019-07-11 15:45:56 +0800

    2019711

--- server/configDB.js
+++ server/configDB.js
@@ -2,6 +2,8 @@ var mongoose=require('mongoose');
 var logger = require('./utils/logger').getLogger();
 //链接MongoDB数据库
  //mongoose.connect('mongodb://libing:123456@127.0.0.1:27017/vj');
+
+
 mongoose.connection.on("connected",function(){
   logger.info('MongoDB 数据库链接成功！');
   // console.log("用户名：libing");
